417e1d0f0d0cc664220914859db5503512f283b0
junbo.li <junbo.li@ff.com>
2019-07-09 18:00:02 +0800

    grid trade strategy

--- examples/no_ui/run.py
+++ examples/no_ui/run.py
@@ -1,33 +1,24 @@
 import multiprocessing
+import signal
 from time import sleep
 from datetime import datetime, time
 from logging import INFO
 
 from vnpy.event import EventEngine
+from vnpy.gateway.huobi import HuobiGateway
 from vnpy.trader.setting import SETTINGS
 from vnpy.trader.engine import MainEngine
 
-from vnpy.gateway.ctp import CtpGateway
+# from vnpy.gateway.ctp import CtpGateway
 from vnpy.app.cta_strategy import CtaStrategyApp
 from vnpy.app.cta_strategy.base import EVENT_CTA_LOG
-
+from vnpy.trader.utility import load_json
 
 SETTINGS["log.active"] = True
 SETTINGS["log.level"] = INFO
 SETTINGS["log.console"] = True
 
-
-ctp_setting = {
-    "用户名": "",
-    "密码": "",
-    "经纪商代码": "",
-    "交易服务器": "",
-    "行情服务器": "",
-    "产品名称": "",
-    "授权编码": "",
-    "产品信息": ""
-}
-
+terminate = False
 
 def run_child():
     """
@@ -37,7 +28,7 @@ def run_child():
 
     event_engine = EventEngine()
     main_engine = MainEngine(event_engine)
-    main_engine.add_gateway(CtpGateway)
+    huobi = main_engine.add_gateway(HuobiGateway)
     cta_engine = main_engine.add_app(CtaStrategyApp)
     main_engine.write_log("主引擎创建成功")
 
@@ -45,8 +36,10 @@ def run_child():
     event_engine.register(EVENT_CTA_LOG, log_engine.process_log_event)
     main_engine.write_log("注册日志事件监听")
 
-    main_engine.connect(ctp_setting, "CTP")
-    main_engine.write_log("连接CTP接口")
+    filename = f"connect_{huobi.gateway_name.lower()}.json"
+    setting = load_json(filename)
+    main_engine.connect(setting, huobi.gateway_name)
+    main_engine.write_log("连接HUOBI接口")
 
     sleep(10)
 
@@ -54,7 +47,7 @@ def run_child():
     main_engine.write_log("CTA策略初始化完成")
 
     cta_engine.init_all_strategies()
-    sleep(60)   # Leave enough time to complete strategy initialization
+    sleep(10)   # Leave enough time to complete strategy initialization
     main_engine.write_log("CTA策略全部初始化")
 
     cta_engine.start_all_strategies()
@@ -70,44 +63,29 @@ def run_parent():
     """
     print("启动CTA策略守护父进程")
 
-    # Chinese futures market trading period (day/night)
-    DAY_START = time(8, 45)
-    DAY_END = time(15, 30)
-
-    NIGHT_START = time(20, 45)
-    NIGHT_END = time(2, 45)
-
     child_process = None
-
-    while True:
-        current_time = datetime.now().time()
-        trading = False
-
-        # Check whether in trading period
-        if (
-            (current_time >= DAY_START and current_time <= DAY_END)
-            or (current_time >= NIGHT_START)
-            or (current_time <= NIGHT_END)
-        ):
-            trading = True
+    signal.signal(signal.SIGINT, kill_handler)
+    while not terminate:
 
         # Start child process in trading period
-        if trading and child_process is None:
+        if child_process is None:
             print("启动子进程")
             child_process = multiprocessing.Process(target=run_child)
             child_process.start()
             print("子进程启动成功")
 
-        # 非记录时间则退出子进程
-        if not trading and child_process is not None:
-            print("关闭子进程")
-            child_process.terminate()
-            child_process.join()
-            child_process = None
-            print("子进程关闭成功")
-
         sleep(5)
 
+    if child_process is not None:
+        print("关闭子进程")
+        child_process.terminate()
+        child_process.join()
+        child_process = None
+        print("子进程关闭成功")
+
+def kill_handler(signal_num, frame):
+    global terminate
+    terminate = True
 
 if __name__ == "__main__":
     run_parent()
---
+++ vnpy/app/cta_strategy/strategies/grid_trade_strategy.py
@@ -0,0 +1,124 @@
+from vnpy.app.cta_strategy import (
+    CtaTemplate,
+    TickData,
+    TradeData,
+    OrderData,
+)
+from vnpy.trader.constant import Status
+
+
+class GridTradeStrategy(CtaTemplate):
+    """"""
+
+    author = "jasion"
+
+    min_diff = 0.01
+    input_ss = 1
+    grid_up_line = 150
+    grid_mid_line = 130
+    grid_dn_line = 120
+    grid_height = 4
+    quote = ""
+
+    base_line = 0
+    entrust = 0
+
+    parameters = ["quote", "min_diff", "input_ss", "grid_up_line",
+                  "grid_mid_line", "grid_dn_line", "grid_height"]
+    variables = ["base_line"]
+
+    def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
+        """"""
+        super(GridTradeStrategy, self).__init__(
+            cta_engine, strategy_name, vt_symbol, setting
+        )
+
+        if self.base_line == 0:
+            self.base_line = self.grid_mid_line
+
+    def on_init(self):
+        """
+        Callback when strategy is inited.
+        """
+        self.write_log("策略初始化")
+
+    def on_start(self):
+        """
+        Callback when strategy is started.
+        """
+        self.write_log("策略启动")
+
+    def on_stop(self):
+        """
+        Callback when strategy is stopped.
+        """
+        self.write_log("策略停止")
+
+    def on_tick(self, tick: TickData):
+        """
+        Callback of new tick data update.
+        """
+        # 首先检查是否是实盘运行还是数据预处理阶段
+        if not self.inited or not self.trading:
+            return
+
+        # 更新持仓
+        base_pos = self.cta_engine.offset_converter.get_position_holding(self.vt_symbol)
+        account = self.cta_engine.main_engine.get_account('.'.join([tick.exchange.value, self.quote]))
+
+        if base_pos is None or account is None:
+            self.write_log(u'获取不到持仓')
+            return
+
+        if tick.last_price > self.grid_up_line or tick.last_price < self.grid_dn_line or self.entrust != 0:
+            self.write_log("price out grid")
+            return
+
+        price = tick.ask_price_1
+
+        if price < (self.base_line / (1 + self.grid_height / 100)):
+            # 买入
+            buy_volume = min(account.balance/price, float(self.input_ss))
+            if buy_volume < self.min_diff:
+                return
+
+            ref = self.buy(price=price, volume=self.input_ss)
+            if ref is not None and len(ref) > 0:
+                self.write_log(u'开多委托单号{},委托买入价：{}'.format(ref, price))
+            else:
+                self.write_log(u'开多委托单失败:{0},v:{1}'.format(price, self.input_ss))
+
+        elif price > (self.base_line * (1 + self.grid_height / 100)):
+            # 卖出
+            sell_volume = min(base_pos.long_pos - base_pos.long_pos_frozen, float(self.input_ss))
+            if sell_volume < self.min_diff:
+                return
+            ref = self.sell(price=price, volume=sell_volume)
+            if ref is not None and len(ref) > 0:
+                self.write_log(u'委托卖出成功, 委托编号:{},委托价格:{}卖出数量{}'.format(ref, price, sell_volume))
+            else:
+                self.write_log(u'委托卖出{}失败,价格:{},数量:{}'.format(self.vt_symbol, price, sell_volume))
+
+
+    def on_order(self, order: OrderData):
+        """
+        Callback of new order data update.
+        """
+        self.write_log(u'报单更新，委托编号:{},合约:{},方向:{},价格:{}委托数量:{},成交:{},状态:{}'
+                         .format(order.orderid, order.symbol,
+                                 order.direction, order.price,
+                                 order.volume,order.traded,
+                                 order.status))
+
+        if order.volume == order.traded or order.status == Status.ALLTRADED:
+            # 开仓，平仓委托单全部成交
+            self.base_line = order.price
+            self.entrust = 0
+        elif order.status in [Status.CANCELLED,Status.REJECTED]:
+            self.entrust = 0
+
+    def on_trade(self, trade: TradeData):
+        """
+        Callback of new trade data update.
+        """
+        self.put_event()
